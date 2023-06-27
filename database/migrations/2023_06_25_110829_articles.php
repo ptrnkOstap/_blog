9e03077c3523cdab2ab7a2beb80cc50df65f23c8
ptrnkOstap <ptrnkostap@gmail.com>
2023-06-27 08:36:54 +0300

    added Article\Product models and migrations for them

---
+++ database/migrations/2023_06_25_110829_articles.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table){
+            $table->id();
+            $table->string('header', 80 )->always();
+            $table->string('text_content')->always();
+            $table->string('attachments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
